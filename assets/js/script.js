a03a464f21109a958c935f712f1f204660005181
Justin Jenkins <justinjnkns1@gmail.com>
2021-05-30 15:46:22 -0400

    added javascript to dynamically update task list

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var saveBtn = document.querySelector('#save-task')
+var tasksToDoE1 = document.querySelector('#tasks-to-do')
+var createTaskHandler = function(){
+    var listItemE1 = document.createElement('li')
+    listItemE1.className = 'task-item'
+    listItemE1.textContent = 'this is a new task'
+    tasksToDoE1.appendChild(listItemE1)
+}
+saveBtn.addEventListener("click",createTaskHandler)
